8d6ef78bc731fbf51982cafc5252228a46aa6568
headneZzz <37807520+headneZzz@users.noreply.github.com>
2019-10-15 18:12:54 +0300

    Add files via upload
    Добавлена кнопка отмены, начата работа на алгоритмом сортировки.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,10 +3,12 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 import javafx.concurrent.Task;
 
 import java.io.*;
-import java.nio.file.Files;
+import java.nio.file.*;
+import java.util.stream.*;
 
 import javafx.stage.DirectoryChooser;
 
@@ -16,23 +18,41 @@ public class Controller {
     private TextField path1;
     @FXML
     private TextField path2;
+    @FXML
+    private Button cancel;
+    @FXML
+    private Button sort;
 
     private final DirectoryChooser directoryChooser = new DirectoryChooser();
 
     @FXML
     protected void SortButtonClicked(ActionEvent event) throws IOException {
-        copyFileUsingStream(new File(path1.getText()),new File(path2.getText()));
+        cancel.setDisable(false);
+        sort.setDisable(true);
+        copyFiles(new File(path1.getText()), new File(path2.getText()));
+    }
+
+    @FXML
+    protected void CancelButtonClicked(ActionEvent event) {
+        cancel.setDisable(true);
+        sort.setDisable(false);
     }
 
 
-    private static void copyFileUsingStream(File source, File dest) throws IOException {
-        Files.copy(new File(source.toString()+"\\1.txt").toPath(),new File(dest.toString()+"\\1.txt").toPath());
+    private static void copyFiles(File source, File dest) throws IOException {
+        Stream<Path> files = Files.list(source.toPath());
+        for (int i = 0; i < files.count(); i++) {
+
+        }
+        String[] parts = source.toString().split("_");
+        Files.copy(new File(source.toString() + "\\1.txt").toPath(), new File(dest.toString() + "\\1.txt").toPath());
     }
 
     @FXML
     protected void Path1ButtonClicked(ActionEvent event) {
         File dir = directoryChooser.showDialog(Main.getPrimaryStage());
         path1.setText(dir.getAbsolutePath());
+
     }
 
     @FXML
@@ -40,4 +60,6 @@ public class Controller {
         File dir = directoryChooser.showDialog(Main.getPrimaryStage());
         path2.setText(dir.getAbsolutePath());
     }
+
+
 }
