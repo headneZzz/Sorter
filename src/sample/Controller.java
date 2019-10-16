b7c272be53137fb30f49270b4842d97fd4ed8439
Илья Крутько <mr.darkilya@mail.ru>
2019-10-16 20:48:18 +0300

    add method

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,15 +4,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
-import javafx.concurrent.Task;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.stream.*;
 import java.util.*;
 
 import javafx.stage.DirectoryChooser;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.nio.file.StandardCopyOption.*;
 
 public class Controller {
     @FXML
@@ -23,14 +29,57 @@ public class Controller {
     private Button cancel;
     @FXML
     private Button sort;
+    private static String[] filesName;
+    private static int THREAD_COUNT = 4;
 
-    private final DirectoryChooser directoryChooser = new DirectoryChooser();
+    class Worker extends Thread {
+        private int threadId;
+
+        private Worker(int id) {
+            threadId = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Started thread:" + threadId);
+            for (int i = threadId; i < filesName.length; i += THREAD_COUNT) {
+                if (filesName[i] != null)
+                    try {
+                        Files.copy(new File(path1.getText() + "\\" + filesName[i]).toPath(), new File(path2.getText() + "\\" + filesName[i]).toPath(), REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                System.out.println(threadId + " Moved:" + path1.getText() + "\\" + filesName[i]);
+            }
+        }
+    }
+
+    private void listFilesForFolder() {
+        int filesCount = 0;
+        try (Stream<Path> files = Files.list(Paths.get(path1.getText()))) {
+            filesCount = (int) files.count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        filesName = new String[filesCount];
+        File direcory = new File(path1.getText());
+        int i = 0;
+        for (final File fileEntry : Objects.requireNonNull(direcory.listFiles())) {
+            if (fileEntry.isFile()) filesName[i] = fileEntry.getName();
+            i++;
+        }
+    }
 
     @FXML
-    protected void SortButtonClicked(ActionEvent event) throws IOException {
+    protected void SortButtonClicked(ActionEvent event) {
         cancel.setDisable(false);
         sort.setDisable(true);
-        //copyFiles(new File(path1.getText()), new File(path2.getText()));
+        listFilesForFolder();
+        System.out.println(Arrays.toString(filesName));
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            Worker w = new Worker(i);
+            w.start();
+        }
     }
 
     @FXML
@@ -39,20 +88,12 @@ public class Controller {
         sort.setDisable(false);
     }
 
-
-    private void copyFiles(File source, File dest) throws IOException {
-        //Stream<Path> files = Files.list(source.toPath());
-        LinkedList<Files> files = new LinkedList<>();
-        //Files.list(source.toPath()).forEach((files.add);
-        String[] parts = source.getName().split("_");
-        Files.copy(new File(source.toString() + files.listIterator()).toPath(), new File(dest.toString() + parts[0] + parts[1] + parts[2] + source.getName()).toPath());
-    }
+    private final DirectoryChooser directoryChooser = new DirectoryChooser();
 
     @FXML
     protected void Path1ButtonClicked(ActionEvent event) {
         File dir = directoryChooser.showDialog(Main.getPrimaryStage());
         path1.setText(dir.getAbsolutePath());
-
     }
 
     @FXML
@@ -60,6 +101,4 @@ public class Controller {
         File dir = directoryChooser.showDialog(Main.getPrimaryStage());
         path2.setText(dir.getAbsolutePath());
     }
-
-
 }
