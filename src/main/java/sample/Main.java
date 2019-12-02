71e485b5b62c68b15119012a60c148fe16438d62
headneZzz <mr.darkilya@mail.ru>
2019-12-02 18:29:33 +0300

    Insert into db

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -7,12 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 public class Main extends Application {
     private static Stage primaryStage;
 
@@ -35,22 +29,7 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://server:5433/archive", "admin", "adminus")) {
-            System.out.println("Connected to database!");
-            Statement statement = connection.createStatement();
-            System.out.println("Reading records...");
-            System.out.printf("%-30.30s  %-30.30s%n", "Код", "Исполнитель");
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM digitization.Исполнители");
-            while (resultSet.next()) {
-                System.out.printf("%-30.30s  %-30.30s%n", resultSet.getString("Код_исполнителя"), resultSet.getString("Исполнитель"));
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Connection failure.");
-            e.printStackTrace();
-        }
         launch(args);
     }
 }
